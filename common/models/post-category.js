e596322a9cbfda6242df391db4188488ac16b79a
Ala Rai <spensa2010alarai@gmail.com>
2018-09-18 14:04:56 +0700

    created and modified date to all model

--- common/models/post-category.js
+++ common/models/post-category.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(Postcategory) {
-
+module.exports = function(Model) {
+  Model.observe('before save', function filterProperties(ctx, next) {
+    if (ctx.isNewInstance) {
+      ctx.instance.createdAt = new Date();
+      ctx.instance.modifiedAt = new Date();
+    } else {
+      ctx.data.modifiedAt = new Date();
+    }
+    next();
+  });
 };
